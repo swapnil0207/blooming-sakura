ccb8dca83cc1e617b9c373ad005f4730d4248704
swapnil0207 <swapnil.j0207@gmail.com>
2021-09-04 21:30:21 +0530

    added changes for header -WIP

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -1,24 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Logo from "../../../public/Assets/Logo/Logo.png";
+import "./Header.css";
+
 
 function Header() {
   return (
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Blooming Sakura</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Header'>
+      {Logo}
     </div>
   );
 }
