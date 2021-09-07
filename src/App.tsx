0a7ca3a1053f91509f20d3d0b4034edab305ea27
swapnil0207 <swapnil.j0207@gmail.com>
2021-09-07 22:22:32 +0530

    Changes for fixed Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import Header from './Common/Header/Header';
-import './App.scss';
+import React from "react";
+import logo from "./logo.svg";
+import Header from "./Common/Header/Header";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className='App'>
-    <Header/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b>Blooming Sakura</b>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </div>
-  );
+	return (
+		<div className="AppMain">
+			<Header />
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						<b>Blooming Sakura</b>
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		</div>
+	);
 }
 
 export default App;
